cb9557a33723966cf52f4788001698872582d9cb
Gregar <126393669+mrd010@users.noreply.github.com>
2023-10-23 20:30:16 +0330

    Add image resource bundling to config

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,12 @@ module.exports = {
     filename: "main.js",
     path: path.resolve("./dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
