85cfe65dfc1d15c8d105fbec292be98683bdbaa5
Gregar <126393669+mrd010@users.noreply.github.com>
2023-10-25 09:16:52 +0330

    Move ElementCreates to ElementCreator.js

---
+++ src/scripts/loaderMain.js
@@ -0,0 +1,7 @@
+import * as Elements from "./scripts/ElementCreator";
+
+export default function createMainPage() {
+  const elementTree = [];
+  elementTree.push(createElement("header", "main-header"));
+  elementTree.push(createElement("div"));
+}
