e35f91dae72a3b65b085415f8c2d6ff60fa7b398
Gregar <126393669+mrd010@users.noreply.github.com>
2023-10-26 16:33:00 +0330

    change mod to production

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
     filename: "main.js",
     path: path.resolve("./dist"),
   },
-  devtool: "inline-source-map",
-  mode: "development",
+  devtool: "source-map",
+  mode: "production",
   module: {
     rules: [
       {
